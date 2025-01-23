e85b8dfbdfab718307c7367129ec2d97a1c02821
KarixD2137 <100468727+KarixD2137@users.noreply.github.com>
2025-01-23 23:43:33 +0100

    23.01.2025
    Kartkówka 4

---
+++ Kartkówka 4 [23.01.2025] - Listy/Kartkowka4/Kartkowka4/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kartkowka4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Przepis> Przepisy { get; set; } = new ObservableCollection<Przepis>();
+
+        public List<string> Kategorie { get; set; }  = new List<string> {"napój","przekąska","danie","słodkie"};
+        public MainWindow()
+        {
+            InitializeComponent();
+            DodajPrzepisy();
+            DataContext = this;
+            DataGridComboBoxKategoria.ItemsSource = Kategorie;
+        }
+
+        public void DodajPrzepisy()
+        {
+            Przepisy.Add(new Przepis("Naleśniki", "słodkie", "mąka, jajka, masło, olej, cukier",5,false));
+            Przepisy.Add(new Przepis("Herbata", "napój", "woda, torebka herbaty, cytryna, cukier", 1, true));
+            Przepisy.Add(new Przepis("Parówki w cieście francuskim", "przekąska", "ciasto francuskie, parówka, ser, ketchup", 12, false));
+            Przepisy.Add(new Przepis("Zupa", "danie", "woda, marchewka, kalafior, śmietana", 4, true));
+        }
+
+        private void DodajPrzepis(object sender, RoutedEventArgs e)
+        {
+            int naIleOsob;
+            if (int.TryParse(tb_na_ile_osob.Text, out naIleOsob))
+            {
+                string nazwa = tb_Nazwa.Text;
+                string kategoria = cmb_Kategoria.Text;
+                string skladniki = tb_skladniki.Text;
+                bool czyBezglutenowe = ckb_czy_bezglutenowe.IsChecked == true ? true : false;
+                Przepisy.Add(new Przepis(nazwa,kategoria, skladniki, naIleOsob, czyBezglutenowe));
+            }
+            else
+            {
+                MessageBox.Show("Ilość osób musi być liczbą całkowitą", "Lista przepisów", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ Kartkówka 4 [23.01.2025] - Listy/Kartkowka4/Kartkowka4/Przepis.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kartkowka4
+{
+    public class Przepis
+    {
+        public string Nazwa { get; set; }
+        public string Kategoria { get; set; }
+        public string Skladniki { get; set; }
+        public int NaIleOsob {  get; set; }
+        public bool CzyBezglutenowe { get; set; }
+
+        public Przepis(string nazwa, string kategoria, string skladniki, int naIleOsob, bool czyBezglutenowe)
+        {
+            Nazwa = nazwa;
+            Kategoria = kategoria;
+            Skladniki = skladniki;
+            NaIleOsob = naIleOsob;
+            CzyBezglutenowe = czyBezglutenowe;
+        }
+    }
+}
