582d431d95a63bcaa760133723331728f9d6ed42
KarixD2137 <100468727+KarixD2137@users.noreply.github.com>
2025-01-23 13:39:58 +0100

    23.01.2025
    Kartkówka 3

---
+++ Kartkówka 3 [11.12.2025] - Galeria zdjęć/Kartkowka3/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kartkowka3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Zdjecie> Zdjecia = new List<Zdjecie>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Zdjecia.Add(new Zdjecie("0.png"));
+            Zdjecia.Add(new Zdjecie("1.png"));
+            Zdjecia.Add(new Zdjecie("2.png"));
+            Zdjecia.Add(new Zdjecie("3.png"));
+        }
+
+        private void PokazZdjęcie(object sender, RoutedEventArgs e)
+        {
+            int idZdjecia;
+            switch (cb_zdjecie.Text)
+            {
+                case "Kwadrat":
+                    idZdjecia = 0;
+                    break;
+                case "Trójkąt":
+                    idZdjecia = 1;
+                    break;
+                case "Koło":
+                    idZdjecia = 2;
+                    break;
+                case "Gwiazda":
+                    idZdjecia = 3;
+                    break;
+                default:
+                    idZdjecia = -1;
+                    break;
+            }
+            if (idZdjecia != -1)
+            {
+                img_zdjecie.Source = new BitmapImage(new Uri(Zdjecia.ElementAt(idZdjecia).sciezka, UriKind.Relative));
+                Zdjecia.ElementAt(idZdjecia).liczbaWyswietlen++;
+                lbl_wyswietlenia.Content = Zdjecia.ElementAt(idZdjecia).liczbaWyswietlen.ToString();
+            }
+        }
+    }
+}
---
+++ Kartkówka 3 [11.12.2025] - Galeria zdjęć/Kartkowka3/Zdjecie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kartkowka3
+{
+    public class Zdjecie
+    {
+        public string sciezka {get; set;}
+        public int liczbaWyswietlen {get; set;}
+
+        public Zdjecie(string sciezka)
+        {
+            this.sciezka = sciezka;
+            this.liczbaWyswietlen = 0;
+        }
+    }
+}
